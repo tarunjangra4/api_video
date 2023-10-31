d4d8f4164acf2aaea972e6d1db4c1809c90b5daf
Tarun Jangra <51834686+tarunjangra4@users.noreply.github.com>
2023-10-31 20:14:02 +0530

    authentication api

---
+++ app.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const authController = require("./controllers/authController");
+const userProfileController = require("./controllers/userProfileController");
+
+// app.use(cors("http://localhost:3000"));
+app.use(cors());
+app.use(express.json()); // it is just a middleware will parse the body into json
+mongoose.connect("mongodb://localhost:27017/video-app");
+
+// Register and login routes from authController
+app.post("/api/register", authController.register);
+app.post("/api/login", authController.login);
+
+// User profile and quote routes from respective controllers
+app.get(
+  "/api/user-profile",
+  authenticateTokenMiddleware,
+  userProfileController.getUserProfile
+);
+app.put(
+  "/api/user-profile",
+  // authenticateTokenMiddleware,
+  userProfileController.updateUserProfile
+);
+
+function authenticateTokenMiddleware(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res
+      .status(401)
+      .json({ status: "error", error: "Token is missing." });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res
+        .status(403)
+        .json({ status: "error", error: "You are not authorized." });
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+// update qoute in the user object
+// app.put("/api/quote", async (req, res) => {
+//   const authHeader = req.headers["authorization"];
+//   const token = authHeader && authHeader.split(" ")[1];
+//   if (!token) {
+//     return res
+//       .status(401)
+//       .json({ status: "error", error: "Token is missing." });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, "secret1234567");
+//     const email = decoded.email;
+//     await User.updateOne({ email: email }, { $set: { quote: req.body.quote } });
+
+//     return res.json({ status: "ok" });
+//   } catch (error) {
+//     return res.json({ status: "error", error: "Invalid token" });
+//   }
+// });
+
+// app.get("/api/quote", async (req, res) => {
+// const authHeader = req.headers["authorization"];
+// const token = authHeader && authHeader.split(" ")[1];
+// if (!token) {
+//   return res
+//     .status(401)
+//     .json({ status: "error", error: "Token is missing." });
+// }
+//   try {
+//     const decoded = jwt.verify(token, "secret1234567");
+//     const email = decoded.email;
+//     const user = await User.findOne({ email: email });
+
+//     return { status: "ok", quote: user.quote };
+//   } catch (error) {
+//     res.json({ status: "error", error: "Invalid token" });
+//   }
+// });
+
+app.listen(3005);
---
+++ controllers/authController.js
@@ -0,0 +1,82 @@
+// const express = require("express");
+// const app = express();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user.model");
+require("dotenv").config();
+
+// sign up api app.post("/api/register",
+exports.register = async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    await User.create({
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    return res
+      .status(200)
+      .json({ status: "ok", message: "User has been created." });
+  } catch (error) {
+    if (error.code === 11000) {
+      // Duplicate key error (email already exists)
+      return res.status(400).json({
+        status: "error",
+        error: "Email already exists. Please use a different email.",
+      });
+    } else {
+      // Other errors
+      return res.status(500).json({
+        status: "error",
+        error: "Registration failed due to an internal server error.",
+      });
+    }
+  }
+};
+
+// login api app.post("/api/login",
+exports.login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+    if (!user) {
+      return res.status(401).json({
+        status: "error",
+        error:
+          "User not found. Please check your email or register a new account.",
+      });
+    } else {
+      console.log(user);
+
+      const isPasswordValid = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      console.log(isPasswordValid);
+      console.log(process.env.ACCESS_TOKEN_SECRET);
+
+      if (isPasswordValid) {
+        const token = jwt.sign(
+          {
+            email: user.email,
+          },
+          process.env.ACCESS_TOKEN_SECRET,
+          { expiresIn: 604800 }
+        );
+        console.log("token");
+        return res.status(200).json({ status: "ok", token });
+      } else {
+        return res
+          .status(401)
+          .json({ status: "error", error: "Password is invalid." });
+      }
+    }
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      error: "Internal server error. Please try again later.",
+    });
+  }
+};
